e99394bc2f6db01fec42b15fbb8677da30de7c15
Huy Nguyen Huynh Minh <huy.nguyenhm@cloudpro.vn>
2022-09-13 13:39:58 +0700

    first commit

---
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * File: LoginController.php
+ * Author: Huy Nguyen
+ * Date: 8/9/2022
+ * Purpose:
+ */
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginController extends Controller
+{   
+
+    // Added by Huy Nguyen on 8/9/2022
+    public function index() {
+        return view('admin.users.login', [
+            'title' => 'Đăng nhập hệ thống',
+        ]);   
+    }
+
+    // Added by Huy Nguyen on 8/9/2022
+    public function store(Request $request) {
+        $this->validate($request, [
+            'email' => 'required|email:filter',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt(['email' => $request->input('email'),'password' => $request->input('password'),], 
+            $request->input('remember'))) {
+            $request->session()->flash('status', 'Task was successful!');
+            
+            return redirect()->route('admin');
+        }
+        
+        return redirect()->back();
+    }
+}
